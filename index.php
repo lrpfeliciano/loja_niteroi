82e778088298a97ae1bdd13da42c2531ddcf069c
lrpfeliciano <lrpfeliciano@gmail.com>
2020-08-06 20:02:36 -0300

    Página inicial incompleta

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bem vindo à nossa loja</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li>Cadastre-se</li>
+			<li>Área Restrita</li>
+		</ul>	
+	</nav>
+
+	Categorias
+	Produtos
+
+</body>
+</html>
